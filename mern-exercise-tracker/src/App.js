be3a5c9cb84b7570b672cc34fb25c59d7f33f557
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2022-09-05 00:29:42 +0530

    React router and design set up completed

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route,Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from './components/navbar.component';
+import ExerciseList from './components/exercises-list.component';
+import EditExercise from './components/edit-exercises.component'
+import CreateExercise from './components/create-exercises.component';
+import CreateUser from './components/create-users.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <br />
+      <Routes>
+     
+        <Route path="/" exact element={<ExerciseList />}></Route>
+        <Route path="/edit/:id" element={<EditExercise />}></Route>
+        <Route path="/create" element={<CreateExercise />}></Route>
+        <Route path="/user" element={<CreateUser />}></Route>
+
+      </Routes>
+
+    </>
+
   );
 }
 
